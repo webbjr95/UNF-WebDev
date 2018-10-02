183327388f35c762789f357d63dab609ad2424a8
webbjr95 <webbjr95@gmail.com>
2018-10-02 14:30:58 -0400

    Add project files.

---
+++ UNF-WebDev/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UNF_WebDev.Startup))]
+namespace UNF_WebDev
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
